d6278e1973786ff2404d8a2137e567ee17fd0101
Gabriel Guesser <gabriel.guesser@zup.com.br>
2021-08-23 11:44:27 -0300

    Criando um Dto para a Categoria

---
+++ src/main/java/br/com/zupacademy/gabriel/casadocodigo/categoria/CategoriaDto.java
@@ -0,0 +1,10 @@
+package br.com.zupacademy.gabriel.casadocodigo.categoria;
+
+public class CategoriaDto {
+
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+}
